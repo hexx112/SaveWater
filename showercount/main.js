dea79919715daaac69ff380ee322a6f77116f3f5
hexx112 <31416064+hexx112@users.noreply.github.com>
2017-09-29 17:56:14 +0100

    base files (lewis)

---
+++ showercount/main.js
@@ -0,0 +1,36 @@
+var final = 90;
+var going = false;
+
+function setup() {
+	document.getElementById("final").innerHTML = final;
+    $( "#endbtn" ).fadeOut( 0, function() {});
+}
+
+function start() {
+    
+    going = true;
+    
+	$( "#startbtn" ).fadeOut( "slow", function() {
+        $("#final").animate({fontSize: "40px" }, 1000 );
+        decrease();
+        $( "#endbtn" ).fadeIn( "slow", function() {});
+    });
+	
+}
+
+function end() {
+    going = false;    
+    $( "#endbtn" ).fadeOut( "slow", function() {
+        $("#final").animate({fontSize: "60px" }, 1000 );
+    });
+}
+
+function decrease(){
+    
+    final = final - 1;
+    document.getElementById("final").innerHTML = final;
+    
+    if (going == true){
+        setTimeout(decrease, 1000);
+    }
+}
