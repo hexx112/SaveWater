b3330d527cab416873cd2a15faacaa3ba805347d
hexx112 <looey2005@gmail.com>
2017-09-29 20:35:14 +0100

    restart button

--- showercount/main.js
+++ showercount/main.js
@@ -1,9 +1,12 @@
-var final = 90;
+var final = 300;
 var going = false;
+var highscorenum = 0;
 
 function setup() {
 	document.getElementById("final").innerHTML = final;
     $( "#endbtn" ).fadeOut( 0, function() {});
+    $( "#highscorecon" ).fadeOut( 0, function() {});
+    $( "#returnbtn" ).fadeOut( 0, function() {});
     document.body.style.zoom="245%"
 }
 
@@ -22,10 +25,21 @@ function start() {
 function end() {
     going = false;    
     $( "#endbtn" ).fadeOut( "slow", function() {
+        
         $("#final").animate({fontSize: "80px" }, 1000 );
+        
         $("#pl").fadeOut(function() {
             $(this).text("You scored:")
         }).fadeIn();
+        
+        $( "#highscorecon" ).fadeIn( "slow", function() {});
+        
+        $( "#returnbtn" ).fadeIn( "slow", function() {});
+        
+        if (final > highscorenum){
+            highscorenum = final;
+            document.getElementById("highscore").innerHTML = highscorenum;
+        }
     });
 }
 
@@ -37,4 +51,21 @@ function decrease(){
     if (going == true){
         setTimeout(decrease, 1000);
     }
+}
+
+function restart(){
+    
+    final = 300;
+    document.getElementById("final").innerHTML = final;});
+    
+    $( "#endbtn" ).fadeOut( 1000, function() {});
+    $( "#highscorecon" ).fadeOut( 1000, function() {});
+    $( "#returnbtn" ).fadeOut( 1000, function() {});
+    $( "#final" ).fadeOut( 1000, function() {});
+    $( "#pl" ).fadeOut( 1000, function() {});
+    
+    $( "#pl" ).fadeIn( 1000, function() {});
+    $( "#final" ).fadeIn( 1000, function() {
+    $( "#startbtn" ).fadeIn( 1000, function() {});
+
 }
