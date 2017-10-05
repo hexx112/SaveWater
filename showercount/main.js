23e308b9d80a54ca71e6b4cffa4b2e39de527564
hexx112 <31416064+hexx112@users.noreply.github.com>
2017-10-05 16:35:44 +0100

    replaced

--- showercount/main.js
+++ showercount/main.js
@@ -1,6 +1,7 @@
 
 //setup variables (global)
-var final = 500;
+var firsttime = 205;
+var final = firsttime;
 var going = false;
 var highscorenum = 0;
 var totalscorenum = 0;
@@ -12,10 +13,11 @@ function setup() {
 	document.getElementById("final").innerHTML = final;
     $( "#endbtn" ).fadeOut( 0, function() {});
     $( "#highscorecon" ).fadeOut( 0, function() {});
-    
+
     $( "#totalscorecon" ).fadeOut( 0, function() {});
     $( "#returnbtn" ).fadeOut( 0, function() {});
-    
+		$( "#coin" ).fadeOut( 0, function() {});
+
     //zoom
     document.body.style.zoom="225%"
 }
@@ -23,60 +25,64 @@ function setup() {
 function start() {
     //on click of start button
     going = true;
-    
+
     //fade out start button
 	$( "#startbtn" ).fadeOut( "slow", function() {
         $("#final").animate({fontSize: "40px" }, 1000 );
         decrease();
     });
-	
+
 }
 
 function end() {
     //change going to false
-    going = false;  
-    
+    going = false;
+
 
     //after fadeout of endbutton
     $( "#endbtn" ).fadeOut( "slow", function() {
-        
+
         $("#final").animate({fontSize: "80px" }, 1000 );
-        
+
         $("#pl").fadeOut(function() {
             $(this).text("You scored:")
         }).fadeIn();
-        
+
         $( "#highscorecon" ).fadeIn( "slow", function() {});
         $( "#totalscorecon" ).fadeIn( "slow", function() {});
-        
+
+				$( "#coin" ).fadeIn( "slow", function() {});
+
         $( "#returnbtn" ).fadeIn( "slow", function() {});
-        
+
         //update highscore
         if (final > highscorenum){
             highscorenum = final;
             document.getElementById("highscore").innerHTML = highscorenum;
         }
-        
+
         //update totalscore
         totalscorenum = final + totalscorenum;
         document.getElementById("totalscore").innerHTML = totalscorenum;
-        
+
     });
-    
+
 }
 
 function decrease(){
+
+
     clickedend = true;
     //decrease
     final = final - 1;
     document.getElementById("final").innerHTML = final;
-    
+
     if (going == true){
         setTimeout(decrease, 1000);
     }
     //turn red
     if (final == 0){
-        
+
         $("*:not(button)").css('background', '#d73232');
     }
     //only add endbtn after 100 secs
@@ -90,26 +96,27 @@ function decrease(){
 function restart(){
     //fade everything out and add only the bits you need back in
     $( "#returnbtn" ).fadeOut( 0000, function() {});
-    
+
     $( "#endbtn" ).fadeOut( 5000, function() {});
-    
+
     $( "#final" ).fadeOut( 1000, function() {
-        final = 300;
+        final = firsttime;
         document.getElementById("final").innerHTML = final;
         $( "#final" ).fadeIn( 1000, function() {});
     });
-        
+
     $( "#pl" ).fadeOut( 1000, function() {
         $("#pl").text("Time remaining:")
         $( "#pl" ).fadeIn( 1000, function() {});
     });
 
     $( "#startbtn" ).fadeIn( 1000, function() {});
-    
+
     $( "#highscorecon" ).fadeIn( 1000, function() {});
     $( "#totalscorecon" ).fadeIn( 1000, function() {});
-    
+
+		$( "#coin" ).fadeOut( 1000, function() {});
     //select all object which arent a button and reset the background colour
-    $("*:not(button)").css('background', '#e6e8e6');
+    $("*:not(button)").css('background', '#256d7b');
 
 }
