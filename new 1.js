563469ecdae8b01bcbe0b785dedbb83cf56094be
zac-mac <32416972+zac-mac@users.noreply.github.com>
2017-11-23 16:03:04 +0000

    Add files via upload

---
+++ new 1.js
@@ -0,0 +1,16 @@
+var jsEncode = {
+	encode: function (s, k) {
+		var enc = "";
+		var str = "";
+		// make sure that input is string
+		str = s.toString();
+		for (var i = 0; i < s.length; i++) {
+			// create block
+			var a = s.charCodeAt(i);
+			// bitwise XOR
+			var b = a ^ k;
+			enc = enc + String.fromCharCode(b);
+		}
+		return enc;
+	}
+};
